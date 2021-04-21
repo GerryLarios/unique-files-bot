7a830e6013bc7490d886a737a899d4b6591363a6
Gerardo Larios <gerardo.larios69@gmail.com>
2021-04-21 17:36:16 -0500

    [ADD] match coincidences

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import argparse
-from utils import isPDF
-from collections import namedtuple
+from utils import is_pdf, is_match, init_container
 
 parser = argparse.ArgumentParser(description="Check a directory if a PDF file is reapeted")
 parser.add_argument("--path", type=str, default=".", help="Directory to clean")
@@ -11,9 +10,6 @@ path = args.path
 
 print(f"Checking {path}")
 
-# create the namedtuple
-Container = namedtuple('Container', ['name', 'size'])
-
 # get all the documents from given directory
 content = os.listdir(path)
 
@@ -21,11 +17,14 @@ content = os.listdir(path)
 path_content = [os.path.join(path, doc) for doc in content]
 
 # filter our directory content into a documents and folders list
-docs = [doc for doc in path_content if os.path.isfile(doc) and isPDF(doc)]
+docs = [init_container(doc) for doc in path_content if is_pdf(doc)]
 
-# go through all files and get the size.
+# go through all files
 for doc in docs:
-    full_doc_path, filetype = os.path.splitext(doc)
-    doc_name = os.path.basename(full_doc_path)
-    current_file = Container(doc_name, os.path.getsize(doc))
-    print(f"{current_file.name} - {current_file.size}BYTES")
+    print(f"> Looking for {doc.name}")
+    matches = [file for file in docs if is_match(doc, file)]
+    if len(matches) == 0:
+        print("Not matches")
+        continue
+    for match in matches:
+        print(f"\t- {match.name}")
--- utils.py
+++ utils.py
@@ -1,4 +1,18 @@
+import os
 import re
+from collections import namedtuple
 
-def isPDF(filetype):
+Container = namedtuple('Container', ['name', 'size', 'path', 'full_path', 'filetype'])
+
+def init_container(doc):
+    full_path, filetype = os.path.splitext(doc)
+    path = os.path.dirname(full_path)
+    name = os.path.basename(full_path)
+    size = os.path.getsize(doc)
+    return Container(name, size, path, full_path, filetype)
+
+def is_pdf(filetype):
     return re.search(r'pdf|PDF', filetype)
+
+def is_match(base_doc, current_doc):
+    return current_doc.size == base_doc.size and current_doc.name is not base_doc.name 
